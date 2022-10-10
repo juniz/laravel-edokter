7358e005a321e981e333c8e36997a91ac24a3461
Yudo <53389941+juniz@users.noreply.github.com>
2022-10-10 14:53:51 +0700

    session dan url

--- config/adminlte.php
+++ config/adminlte.php
@@ -200,7 +200,7 @@ return [
     */
 
     'use_route_url' => false,
-    'dashboard_url' => 'home',
+    'dashboard_url' => false,
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => 'register',
