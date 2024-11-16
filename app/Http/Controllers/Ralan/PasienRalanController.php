7ce88bf30ddf954c3f50fb16bc0a8c049afc836f
Yudo <53389941+juniz@users.noreply.github.com>
2024-11-16 13:46:32 +0700

    pasien dokter anastesi pasien ralan igd

--- app/Http/Controllers/Ralan/PasienRalanController.php
+++ app/Http/Controllers/Ralan/PasienRalanController.php
@@ -29,20 +29,33 @@ class PasienRalanController extends Controller
     {
         $kd_poli = session()->get('kd_poli');
         $kd_dokter = session()->get('username');
+        $kd_sps = session()->get('kd_sps');
         $tanggal = Request::get('tanggal') ?? date('Y-m-d');
         $heads = ['No. Reg', 'Nama Pasien', 'No Rawat', 'Telp', 'Dokter', 'Status'];
         $headsInternal = ['No. Reg', 'No. RM', 'Nama Pasien', 'Dokter', 'Status'];
-        $data = DB::table('reg_periksa')
-            ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
-            ->join('dokter', 'dokter.kd_dokter', '=', 'reg_periksa.kd_dokter')
-            ->leftJoin('resume_pasien', 'reg_periksa.no_rawat', '=', 'resume_pasien.no_rawat')
-            ->where('reg_periksa.kd_poli', $kd_poli)
-            ->where('tgl_registrasi', $tanggal)
-            ->where('reg_periksa.kd_dokter', $kd_dokter)
-            ->orderBy('reg_periksa.jam_reg', 'desc')
-            ->select('reg_periksa.no_reg', 'pasien.nm_pasien', 'reg_periksa.no_rawat', 'pasien.no_tlp', 'dokter.nm_dokter', 'reg_periksa.stts', 'pasien.no_rkm_medis', 'resume_pasien.diagnosa_utama')
-            ->get();
-
+        if ($kd_dokter == '86062112' || $kd_dokter == 'SP0000005' || $kd_dokter == 'SP0000002' || $kd_dokter == 'SP0000006' || $kd_sps == 'S004') {
+            $data = DB::table('reg_periksa')
+                ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+                ->join('dokter', 'dokter.kd_dokter', '=', 'reg_periksa.kd_dokter')
+                ->leftJoin('resume_pasien', 'reg_periksa.no_rawat', '=', 'resume_pasien.no_rawat')
+                ->where('reg_periksa.kd_poli', $kd_poli)
+                ->where('tgl_registrasi', $tanggal)
+                // ->where('reg_periksa.kd_dokter', $kd_dokter)
+                ->orderBy('reg_periksa.jam_reg', 'desc')
+                ->select('reg_periksa.no_reg', 'pasien.nm_pasien', 'reg_periksa.no_rawat', 'pasien.no_tlp', 'dokter.nm_dokter', 'reg_periksa.stts', 'pasien.no_rkm_medis', 'resume_pasien.diagnosa_utama')
+                ->get();
+        } else {
+            $data = DB::table('reg_periksa')
+                ->join('pasien', 'reg_periksa.no_rkm_medis', '=', 'pasien.no_rkm_medis')
+                ->join('dokter', 'dokter.kd_dokter', '=', 'reg_periksa.kd_dokter')
+                ->leftJoin('resume_pasien', 'reg_periksa.no_rawat', '=', 'resume_pasien.no_rawat')
+                ->where('reg_periksa.kd_poli', $kd_poli)
+                ->where('tgl_registrasi', $tanggal)
+                ->where('reg_periksa.kd_dokter', $kd_dokter)
+                ->orderBy('reg_periksa.jam_reg', 'desc')
+                ->select('reg_periksa.no_reg', 'pasien.nm_pasien', 'reg_periksa.no_rawat', 'pasien.no_tlp', 'dokter.nm_dokter', 'reg_periksa.stts', 'pasien.no_rkm_medis', 'resume_pasien.diagnosa_utama')
+                ->get();
+        }
 
         return view('ralan.pasien-ralan', [
             'nm_poli' => $this->getPoliklinik($kd_poli),
